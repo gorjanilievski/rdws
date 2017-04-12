0d54eff448c344493194baaf85d9762a8402d228
gorjan.ilievski <gorjan.ilievski@GorjanILap-tp.ITGMA-CORP>
2017-04-12 18:07:13 +0200

    Domasnа za cas8

---
+++ Domasno8.php
@@ -0,0 +1,58 @@
+<?php
+echo '1. За сите овие SQL зборчиња да напишете по еден SQL каде ќе го искористите секој од нив:'
+. '- SELECT
+- FROM
+- WHERE
+- AND / OR
+- LIKE / NOT LIKE
+- COUNT
+- AS
+- IN / NOT IN
+- UPDATE
+- SET
+- INSERT INTO
+- DELETE
+- LIMIT
+- BETWEEN
+- DROP
+- TRUNCATE
+- EXPORT / IMPORT
+- ORDER BY
+- GROUP BY
+- ASC / DESC
+<br/>';
+echo '2. Комитајте сите за дизајнот на едно место, за да можеме целосно да го анализираме.<br/>';
+echo '3. Подолу е дадена листата за базите што ја направивме денес за сајтот:';
+
+?>
+
+Sigurno:
+1. apliciraj graficki/veb.jpg
+Apliciranje
+- id, ime, prezime, telefon, email, akademija, datum_na_apliciranje
+2. homefinal2.jpg
+Nastani
+- id, datum, ime, opis, lokacija, coordX, coordY 
+3. proekti graficki/veb.jpg
+Proekti
+- id, generacija, akademija, student?, klient, predmeti, ime, datum, skills, url
+4. profesorifinal.jpg
+Profesori
+- id, ime, prezime, opis, akademija, slika
+5. programa graficki/veb.jpg
+Predmeti
+- id, ime_predmet, opis, profesor, broj_na_casovi, semestar
+6. studenti web.jpg
+Studenti
+- id, ime, prezime, slika, generacija, akademija, opis, quote, sertifikati?
+
+
+Potencijalno:
+1. about.jpg
+- Osobini na akademija
+- Kontakt
+- Delovni sorabotnici
+2. faq graficki
+- faq
+3. homefinal2.jpg
+- opstestveno odgovorni praktiki
